36e5b2f9fac4fe3d8cb115028277b98302853496
Simon Fridlund <simon.r.k.fridlund@gmail.com>
2014-08-26 22:28:27 +0200

    Migrate to new package system
    Change name of package

--- package.js
+++ package.js
@@ -1,12 +1,16 @@
 Package.describe({
+  git: 'https://github.com/zimme/meteor-iron-router-auth.git',
+  name: 'zimme:iron-router-auth',
   summary: 'Auth hook for iron-router',
   version: '0.0.8'
 });
 
 Package.on_use(function (api, where) {
+  api.versionsFrom('METEOR@0.9.0');
+
   api.use('coffeescript', 'client');
   api.use('underscore', 'client');
-  api.use('iron-router', 'client');
+  api.use("cmather:iron-router@0.8.2", 'client');
 
   api.add_files('hooks.coffee', 'client');
 });
