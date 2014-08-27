a4fda13676afb7151fa135a05e6fed54087378ed
zimme <zimme@users.noreply.github.com>
2014-08-27 12:21:01 +0200

    Update dependencies

--- package.js
+++ package.js
@@ -8,9 +8,10 @@ Package.describe({
 Package.onUse(function (api, where) {
   api.versionsFrom('0.9.0');
 
+  api.use('accounts-base', 'client');
   api.use('coffeescript', 'client');
+  api.use('iron:router@0.9.0', 'client');
   api.use('underscore', 'client');
-  api.use("iron:router@0.9.0", 'client');
 
   api.addFiles('hooks.coffee', 'client');
 });
