ace6e82e921e9bfdcb6db88427c4ba841f4f84e7
Simon Fridlund <simon.r.k.fridlund@gmail.com>
2014-09-04 17:47:36 +0200

    Version bump
    Bump versions and update versions.js

--- package.js
+++ package.js
@@ -2,15 +2,15 @@ Package.describe({
   git: 'https://github.com/zimme/meteor-iron-router-auth.git',
   name: 'zimme:iron-router-auth',
   summary: 'Auth hook for iron-router',
-  version: '0.1.1'
+  version: '0.1.2-rc1'
 });
 
 Package.onUse(function (api, where) {
-  api.versionsFrom('0.9.0');
+  api.versionsFrom('0.9.1-rc3');
 
   api.use('accounts-base', 'client');
   api.use('coffeescript', 'client');
-  api.use('iron:router@0.9.0', 'client');
+  api.use('iron:router@0.9.2-rc2', 'client');
   api.use('underscore', 'client');
 
   api.addFiles('hooks.coffee', 'client');
