6c654a35dfb29a09d4fb9b2adb3e46b732277bf5
Simon Fridlund <simon.r.k.fridlund@gmail.com>
2015-11-23 21:58:39 +0100

    Major version bump

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   git: 'https://github.com/zimme/meteor-iron-router-auth.git',
   name: 'zimme:iron-router-auth',
   summary: 'Authentication and authorization for iron:router',
-  version: '3.2.0-rc.4'
+  version: '4.0.0-alpha.1',
 });
 
 Package.onUse(function (api) {
